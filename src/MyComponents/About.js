b80815ddf9462322aab03f0a8804db940da5a839
Deep <deepbanerjee68@gmail.com>
2021-05-28 00:07:44 +0530

    Initial commit

---
+++ src/MyComponents/About.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            About This Project
+            <p>
+                 This a Todo list project app where you can add and delete your task
+                 this application completely made on react Js
+                 this project is made for learling react
+                 this project made by:- Deep Banerjee
+                 from guru nanak institute of technology,3rd year,ECE
+             </p>
+        </div>
+    )
+}
